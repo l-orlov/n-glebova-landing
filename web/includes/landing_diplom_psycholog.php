bd20fc2c9155ba5f3bd4733928e0977a0c02b3dd
Andrey Gusev <andrey.gusev.dev@gmail.com>
2025-08-24 18:16:27 -0300

    Fix diplom psycholog

--- web/includes/landing_diplom_psycholog.php
+++ web/includes/landing_diplom_psycholog.php
@@ -1,9 +1,10 @@
 <div class="diplom_psycholog">
   <div class="diplom_psycholog_inner">
-    <img src="img/diplom_psycholog.jpg" alt="Наталья Глебова" class="diplom_psycholog_photo">
+    <div class="diplom_psycholog_photo_wrap">
+      <img src="img/diplom_psycholog.jpg" alt="Наталья Глебова" class="diplom_psycholog_photo">
+    </div>
 
     <h2 class="diplom_psycholog_name">Наталья Глебова</h2>
-
     <p class="diplom_psycholog_text">
       Дипломированный психолог по профилю психоанализ. В психологии более 10 лет.
       С 2015 года изучаю психоанализ и практикую в рамках психоаналитической психотерапии.
